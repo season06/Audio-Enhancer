a9464abb28d38a29181ee1cd3e31880b79a4739c
season06 <mimi871006@gmail.com>
2022-05-31 14:28:08 +0800

    feat: test fft performance

--- tests/test_fft.py
+++ tests/test_fft.py
@@ -3,8 +3,10 @@ sys.path.append(os.getcwd())
 
 import main
 import pytest
+import timeit
 import numpy as np
-from scipy.fftpack import fft, ifft
+from scipy.fftpack import fft
+
 
 def test_padding():
     n_size = [3, 64, 400, 1025, 200000]
@@ -48,3 +50,27 @@ def test_ifft():
 
         for i in range(0, n):
             assert abs(data[i] - real[i]) <= 1e-3
+
+
+def test_performance():
+    max = 32767
+    n_size = [1024, 2048, 4096, 8192, 16384, 32768, 65536]
+
+    with open('performance.txt', 'w') as f:
+        f.write("Performance for FFT\n")
+
+        for n in n_size:
+            data = np.random.randint(max, size=n, dtype=np.int16).tolist()
+            comp = main.initialize(data)
+            self_init = dict(main=main, comp=comp, n=n)
+            py_init = dict(fft=fft, data=data)
+
+            f.write(f"\nsize: {str(n)}\n")
+
+            self_fft_time = timeit.timeit("main.fft(comp, n)", number=1, globals=self_init)
+            f.write(f"Self FFT time : {str(self_fft_time)}\n")
+
+            py_fft_time = timeit.timeit("fft(data)", number=1, globals=py_init)
+            f.write(f"Scipy FFT time: {str(py_fft_time)}\n")
+
+        f.close()
